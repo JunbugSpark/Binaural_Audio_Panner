27caad8eb2544e9a449cb8da533ee3c86441d1d4
JunbugSpark <parkseojun118@gmail.com>
2026-07-23 15:10:34 +0900

    Add files via upload

---
+++ PF1_BinauralPanner_2.2.py
@@ -0,0 +1,103 @@
+##Overlap-Add Convolution
+##Goal is to speed up the 1.1 convolution process
+
+import numpy as np                  ##Core DSP math library
+import scipy.signal as signal       ##Contains pre-built DSP functions
+import soundfile as sf              ##Reads and writes .wav files
+import matplotlib.pyplot as plt     ##Allows you to plot and visualize
+import os                           ##File path handling
+import sounddevice as sd            ##Crucial for real time audio
+from scipy.io import wavfile        ##For reading .wav files
+from scipy.signal import resample_poly  ##For changing HRTF sample rate to 48kHz
+import math                         ##For rounding up during the for loop
+
+import zipfile                      ##For unzipping HRTF dataset
+
+##Extract HRTF dataset
+zipfile.ZipFile('/Users/junbug/Documents/VS Code/Python/HRTF.zip').extractall('/Users/junbug/Documents/VS Code/Python')
+
+##Read sample audio file - Lil Uzi Vert
+fs, xsig = wavfile.read('/Users/junbug/Documents/VS Code/Python/XO Tour Llif3.wav')
+x = np.array(xsig)
+
+##Assign HRTF parameters
+elevation = 20
+pinna = "H"
+azimuth = 180
+
+##Assign HRTF filename based on parameters
+filename = f"elev{elevation}/{pinna}{elevation}e{azimuth:03d}a.wav"
+
+##Read the HRTF file
+hfs, hsig = wavfile.read('/Users/junbug/Documents/VS Code/Python/' + filename)
+
+##Resample the HRTF to match audio file sample rate
+h_sig = resample_poly(hsig, fs, hfs)
+
+##Divide the HRTF into left and right channels
+h = np.array(h_sig)
+
+h_left = h[:, 0]
+h_right = h[:, 1]
+
+##Stereo Left Convolve
+def left_convolve(x, h):
+  ##Initialize array lengths
+  x_length = len(x)
+  h_length = len(h)
+  y_length = x_length + h_length - 1
+
+  ##Array that output will go into
+  y = np.zeros(y_length)
+
+  ##Chunk size inputs, decided by user depending on needs
+  chunkSize = 1024
+  index = 0
+  
+  for n in range(math.ceil(x_length / chunkSize)):
+
+    chunk = x[index : index + chunkSize]
+
+    leftProduct = signal.fftconvolve(chunk, h_left, mode='full')
+
+    y[index : index + len(leftProduct)] += leftProduct
+
+    index = index + chunkSize
+
+  return y
+
+##Stereo-right convolve
+def right_convolve(x, h):
+  x_length = len(x)
+  h_length = len(h)
+  y_length = x_length + h_length - 1
+
+  y = np.zeros(y_length)
+
+  chunkSize = 1024
+  index = 0
+  
+  for n in range(math.ceil(x_length / chunkSize)):
+
+    chunk = x[index : index + chunkSize]
+
+    leftProduct = signal.fftconvolve(chunk, h_right, mode='full')
+
+    y[index : index + len(leftProduct)] += leftProduct
+
+    index = index + chunkSize
+
+  return y
+
+y_left = left_convolve(x, h)
+y_right = right_convolve(x, h)
+
+output = np.zeros((len(y_left), 2))
+
+output[:, 0] = y_left
+output[:, 1] = y_right
+
+output = output / np.max(np.abs(output))
+
+sd.play(output, fs)
+sd.wait()
